7985868ee0a2cc68ba69be42c351f8a913bd2cfe
Martin Krcho <martin@wpwhitesecurity.com>
2021-03-18 13:54:51 +0100

    Simplify notice message translation.

--- core/class-extension-core.php
+++ core/class-extension-core.php
@@ -147,10 +147,11 @@ if ( ! class_exists( '\WPWhiteSecurity\ActivityLog\Extensions\Common\Core' ) ) {
                 <div class="notice notice-success is-dismissible wsal-installer-notice">
 					<?php
 					printf(
-						'<p>%1$s %2$s %3$s &nbsp;&nbsp;<button class="install-wsal button button-primary" data-plugin-slug="wp-security-audit-log/wp-security-audit-log.php" data-plugin-download-url="%4$s" data-plugins-network="%6$s" data-nonce="%5$s">%7$s</button><span class="spinner" style="display: none; visibility: visible; float: none; margin: 0 0 0 8px;"></span></p>',
-						esc_html__( 'The', 'wsal-extension-core' ),
-						$this->extention_plugin_name,
-						esc_html__( 'extension requires the WP Activity Log plugin to work.', 'wsal-extension-core' ),
+						'<p>%1$s &nbsp;&nbsp;<button class="install-wsal button button-primary" data-plugin-slug="wp-security-audit-log/wp-security-audit-log.php" data-plugin-download-url="%2$s" data-plugins-network="%4$s" data-nonce="%3$s">%5$s</button><span class="spinner" style="display: none; visibility: visible; float: none; margin: 0 0 0 8px;"></span></p>',
+						sprintf(
+							esc_html__( 'The %s extension requires the WP Activity Log plugin to work.', 'wsal-extension-core' ),
+							$this->extention_plugin_name
+                        ),
 						esc_url( 'https://downloads.wordpress.org/plugin/wp-security-audit-log.latest-stable.zip' ),
 						esc_attr( wp_create_nonce( 'wsal-install-addon' ) ),
 						( is_a( $screen, '\WP_Screen' ) && isset( $screen->id ) && 'plugins-network' === $screen->id ) ? true : false, // confirms if we are on a network or not.
