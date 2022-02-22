01d176632e4419ad972d435a96d9d48efdfc1cdb
Danny Jones <daniel@wpwhitesecurity.com>
2022-02-22 16:52:33 +0000

    Coding standards

--- wp-security-audit-log/custom-alerts.php
+++ wp-security-audit-log/custom-alerts.php
@@ -1,17 +1,22 @@
 <?php
 /**
- * Use this file to register your custom events.
+ * Our list of events.
+ *
+ * @package wsal
  */
 
+// phpcs:disable WordPress.WP.I18n.UnorderedPlaceholdersText 
+// phpcs:disable WordPress.WP.I18n.MissingTranslatorsComment
+
 $custom_alerts = array(
-	__( 'PLUGINNAME', 'wp-security-audit-log' ) => array(
-		__( 'PLUGINNAME Content', 'wp-security-audit-log' ) => array(
+	esc_html__( 'PLUGINNAME', 'wp-security-audit-log' ) => array(
+		esc_html__( 'PLUGINNAME Content', 'wp-security-audit-log' ) => array(
 
 			array(
 				1234,
 				WSAL_LOW,
-				__( 'Test event X', 'wp-security-audit-log' ),
-				__( 'This is a test event', 'wp-security-audit-log' ),
+				esc_html__( 'Test event X', 'wp-security-audit-log' ),
+				esc_html__( 'This is a test event', 'wp-security-audit-log' ),
 				'wpforms',
 				'created',
 			),
